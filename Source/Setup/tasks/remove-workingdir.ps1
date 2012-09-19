837e50ae7950b45781aeb737ee12ffbd952ac9d5
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2012-09-19 17:58:46 -0300

    # Removed all orphan ps1 scripts

--- Source/Setup/tasks/remove-workingdir.ps1
+++
@@ -1,9 +0,0 @@
-Param([string] $demoWorkingDir)
-
-write-host "========= Removing current working directory... ========="
-if (Test-Path "$demoWorkingDir")
-{
-	Remove-Item "$demoWorkingDir" -recurse -force
-}
-
-write-host "Removing current working directory done!"
